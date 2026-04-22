623ef77e3456bba07041c7ee7d5b66cbe23008cc
TheMasked <87376667+TheMasked2@users.noreply.github.com>
2026-04-22 16:06:06 +0200

    MongoDB added
    Added MongoDB & Mongo-Express to the docker-compose file, effectively implementing a NoSQL database into the project.

--- backend/turtleshop-api/build.gradle.kts
+++ backend/turtleshop-api/build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
+    // NoSQL - MongoDB
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
