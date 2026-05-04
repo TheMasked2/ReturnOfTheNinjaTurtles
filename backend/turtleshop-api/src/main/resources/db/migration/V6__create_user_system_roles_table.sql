3973a7338e9eda2c1ade1b287feff0ad7606f2dd
PieterD <87376667+TheMasked2@users.noreply.github.com>
2026-05-04 23:47:40 +0200

    fix foreign key constraints
    fix bug where other tables depended on customer_id being an INT instead of UUID

--- backend/turtleshop-api/src/main/resources/db/migration/V6__create_user_system_roles_table.sql
+++ backend/turtleshop-api/src/main/resources/db/migration/V6__create_user_system_roles_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS USER_SYSTEM_ROLES (
-    customer_id INT,
+    customer_id UUID,
     role_id INT,
     PRIMARY KEY (customer_id, role_id),
     CONSTRAINT fk_user_role_customer FOREIGN KEY (customer_id) REFERENCES CUSTOMER(customer_id),
