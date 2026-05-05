eb1724e12e0ac6f0865747ed9335b4133d6cf962
EyeMustache <lucioridder2003@gmail.com>
2026-05-05 17:03:16 +0200

    Made wishlist and -item models

---
+++ backend/turtleshop-api/src/main/java/org/turtleshop/api/modules/wishlist/model/WishList.java
@@ -0,0 +1,17 @@
+package org.turtleshop.api.modules.wishlist.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+// import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class WishList {
+    private Integer wishlistId; // PK
+    private UUID customerId; // FK
+    // private List<WishListItem> items; // Potential future use
+}
