f670726df96a84361e234236d8930a1e58f01c21
PieterD <87376667+TheMasked2@users.noreply.github.com>
2026-05-11 20:22:56 +0200

    Update inventory table initialization
    it now matches ERD

--- backend/turtleshop-api/src/main/resources/db/migration/V8__create_inventory_table.sql
+++ backend/turtleshop-api/src/main/resources/db/migration/V8__create_inventory_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS INVENTORY (
     inventory_id INT PRIMARY KEY,
     product_id INT,
     quantity_available INT DEFAULT 0,
+    quantity_reserved INT DEFAULT 0,
     last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_inventory_product FOREIGN KEY (product_id) REFERENCES PRODUCT(product_id)
 );
