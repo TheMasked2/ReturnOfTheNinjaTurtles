eb33859e77bbae29f751fe136c4f2369fca1ce63
EyeMustache <lucioridder2003@gmail.com>
2026-06-16 14:09:31 +0200

    Removed excess seeding files

--- backend/seeder/seed_data.py
+++ backend/seeder/seed_data.py
@@ -28,7 +28,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")  # External mapped port from compose
+DB_PORT = os.getenv("DB_PORT")
 
 # MongoDB Connection Parameters
 MONGO_USER = os.getenv("MONGO_USER")
